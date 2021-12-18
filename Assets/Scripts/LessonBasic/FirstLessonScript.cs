c59f1d3b4836c9ea7cb2b8a0d6367a1fc6a9a1a4
Dogancan Cedetas <dogancancedetas@hotmail.com>
2021-12-19 00:37:10 +0300

    +added: new rotate script +refactor: basic lesson: added vector3

--- Assets/Scripts/LessonBasic/FirstLessonScript.cs
+++ Assets/Scripts/LessonBasic/FirstLessonScript.cs
@@ -17,6 +17,8 @@ namespace LessonBasic
         bool dogru = true;
         bool yanlis = false;
 
+        [SerializeField] private Vector3 ucFloat = new Vector3(1, 2, 3);
+
         // Start is called before the first frame update
         void Start()
         {
---
+++ Assets/Scripts/Lessons/RotateObject.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Lessons
+{
+    public class RotateObject : MonoBehaviour
+    {
+        [SerializeField] private float _speed = 1;
+
+        private void Update()
+        {
+            transform.Rotate(Vector3.up, _speed * Time.deltaTime);
+            //100 frame per second 100 * 0.1 = 10 /////////// 50 frame per second 50 * 0.2 = 10
+        }
+    }
+}
