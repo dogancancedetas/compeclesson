1f327d76e9d6ee522e74fdf21e5100173a5616ed
Dogancan Cedetas <dogancancedetas@hotmail.com>
2021-12-19 00:23:29 +0300

    +created first lesson script

---
+++ Assets/Scripts/FirstLessonScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LessonBasic
+{
+    public class FirstLessonScript : MonoBehaviour
+    {   /// comment yani herhangi bir þey
+        /// <summary>
+        /// C# variables
+        /// </summary>
+
+        public int tamSayi = 1; //integar
+        [SerializeField] int tamSayi2 = 100;
+        float ondalikSayi = 0.25f;
+        float ondalikSayi2 = 5.2342f;
+        bool dogruYanlis = true;
+        bool dogruYanlis2 = false;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
